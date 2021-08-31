5c6f7733e9f9be0c25c9f8b201d0cad722e7382d
pa-prajith <codejs100@gmail.com>
2021-08-31 14:26:00 -0700

    Add code to use event deligation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
   const imgElements = document.querySelectorAll(".panel");
   let activeElement = imgElements[0];
   const clickHandler = function () {
@@ -16,4 +16,30 @@
   };
 
   imgElements.forEach((ele) => ele.addEventListener("click", clickHandler));
+})();*/
+
+(function () {
+  const containerEle = document.querySelector(".container");
+  let activeElement = document.querySelector(".active");
+
+  const divClickHandler = function () {
+    if (activeElement) {
+      if (activeElement.classList.contains("active")) {
+        activeElement.classList.remove("active");
+      }
+    }
+    if (activeElement !== this) {
+      activeElement = this;
+      this.classList.add("active");
+    } else {
+      activeElement = null;
+    }
+  };
+
+  containerEle.addEventListener("click", (e) => {
+    if (e.target.classList.contains("panel")) {
+      divClickHandler.call(e.target);
+    }
+    return false;
+  });
 })();
